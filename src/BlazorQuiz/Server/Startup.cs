cfcc2b79a90a3e65467b9b03a27707ee34b14bc4
Steven,Yeun <mryeun@naver.com>
2022-02-05 02:22:38 +0900

    almost finish

--- src/BlazorQuiz/Server/Startup.cs
+++ src/BlazorQuiz/Server/Startup.cs
@@ -1,3 +1,4 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -22,7 +23,7 @@ namespace BlazorQuiz.Server
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-
+			services.AddMatBlazor();
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
